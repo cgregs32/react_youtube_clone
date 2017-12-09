4be32b020dfa0640230f6bb05ba2a27aa0e489c2
cgregs32 <cgregs32@gmail.com>
2017-12-09 15:36:25 -0700

    added lodash and searching functions

--- src/index.js
+++ src/index.js
@@ -1,13 +1,50 @@
+import _ from 'lodash';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyDfwGMWuJZ8nfEoqPQHMtWmYuetAyK6bvc';
 
-const App = () => (
 
-  <div>Hi!</div>
+class App extends React.Component{
+  constructor(props){
+    super(props);
 
-)
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+    this.videoSearch('climbing');
+  }
 
 
-ReactDOM.render(<App />, document.querySelector('.container'))
+  videoSearch = (term) => {
+    YTSearch({ key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+
+    const videoSearch =  _.debounce((term) => {this.videoSearch(term) }, 500);
+
+    return(
+      <div>
+        <SearchBar onSearchTermChange={videoSearch}/>
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
